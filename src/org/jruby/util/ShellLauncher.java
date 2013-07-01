b9aed41fe134bd84b78ffc307bf7420137216160
David Pershouse <dpershouse@gmail.com>
2013-07-01 14:01:15 +1000

    first pass of fixes to runAndWait

--- src/org/jruby/util/ShellLauncher.java
+++ src/org/jruby/util/ShellLauncher.java
@@ -540,9 +540,27 @@ public class ShellLauncher {
         OutputStream error = runtime.getErrorStream();
         InputStream input = runtime.getInputStream();
         try {
-            Process aProcess = run(runtime, rawArgs, doExecutableSearch);
-            handleStreams(runtime, aProcess, input, output, error);
-            return new long[] {aProcess.waitFor(), getPidFromProcess(aProcess)};
+            aProcess = run(runtime, rawArgs, doExecutableSearch);
+
+            InputStream pOut = aProcess.getInputStream();
+            InputStream pErr = aProcess.getErrorStream();
+            OutputStream pIn = aProcess.getOutputStream();
+
+            StreamPumper t1 = new StreamPumper(runtime, pOut, output, false, Pumper.Slave.IN, aProcess);
+            StreamPumper t2 = new StreamPumper(runtime, pErr, error, false, Pumper.Slave.IN, aProcess);
+            StreamPumper t3 = new StreamPumper(runtime, input, pIn, true, Pumper.Slave.OUT, aProcess);
+
+            t1.start();
+            t2.start();
+            t3.start();
+            try {
+                return new long[] {aProcess.waitFor(), getPidFromProcess(aProcess)};
+            } finally {
+                t1.quit();
+                t2.quit();
+                t3.quit();
+                t3.stop(); //stinky STDIN is blocking
+            }
         } catch (IOException e) {
             throw runtime.newIOErrorFromException(e);
         } catch (InterruptedException e) {
